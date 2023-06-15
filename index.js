089aa29df7465bcbb1255991b1774e4cbbe419e9
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-06-15 09:59:42 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+
+function writeCards(name, eventName) {
+    let cardArray = []
+    for(let i = 0; i < name.length; i++) {
+        cardArray.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    } 
+    return cardArray
+}
+
+// function countDown(number) {
+//     for(let i = number.length; i < number.length; i++) { 
+//         console.log(number[i])
+//     }
+   
+// }
+
+function countDown(number) {
+    for(let i = number; i >= 0; i--) { 
+        console.log(i);
+    }
+}
+
+// console.log(countDown(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']))
